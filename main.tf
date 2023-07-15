c940fee6d3157c68430da9d924ea14098913037e
Joao Gabriel <joaogabriel.ovalle@gmail.com>
2023-07-15 13:00:45 -0300

    Update  de modulos

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+provider "aws" {
+  alias  = "west-2"
+  region = "us-west-2"
+
+}
+
+terraform {
+  backend "s3" {
+    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
+    bucket = "poc-terraform-jgabriel01"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
